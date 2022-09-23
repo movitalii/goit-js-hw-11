298d1be03e1bf2f548470fe6837ceb93176440a7
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-23 22:33:15 +0300

    created SCSS

--- src/index.js
+++ src/index.js
@@ -1,53 +1,40 @@
-const API_KEY = '30062649-6c95f8a5f26546f2640c7031e';
-const BASE_URL = 'https://pixabay.com/api/';
-
 const formEl = document.querySelector('#search-form');
-const divEl = document.querySelector('div');
+const divEl = document.querySelector('.gallery');
 formEl.addEventListener('submit', onFormSubmit);
-
+import { fetchPhotos } from './fetchPhotos';
 
 function onFormSubmit(event) {
     event.preventDefault();
 
-    const {
-    elements: { searchQuery }
-  } = event.currentTarget;
+    const data = event.currentTarget.elements.searchQuery.value;
 
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery.value}&image_type=photo`)
-    .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
-            return response.json();
-    })
-    .then(renderCards)
+    fetchPhotos(data)
+    .then(data => { 
+        const elements = Object.entries(data);
+        const photosInfo = elements[elements.length - 1];
+        const photos = photosInfo[photosInfo.length - 1];
+
+        const markup = photos.map((photo) => {
+          return `<div class="photo-card">
+          <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+          <div class="info">
+            <p class="info-item">
+              <b>Likes</b>
+            </p>
+            <p class="info-item">
+              <b>${photo.views}</b>
+            </p>
+            <p class="info-item">
+              <b>${photo.comments}</b>
+            </p>
+            <p class="info-item">
+              <b>${photo.downloads}</b>
+            </p>
+          </div>
+        </div>`;
+        })
+        .join("");
+        divEl.innerHTML = markup;
+      })      
     .catch(error => console.log(error))
 }
-
-function renderCards(cards) {
-    const markup = cards
-        .map((card) => {
-            return `
-                <div class="photo-card">
-                  <img src="${card.webformatURL}" alt="${card.tags}" loading="lazy" />
-                  <div class="info">
-                    <p class="info-item">
-                      <b>Likes</b>
-                    </p>
-                    <p class="info-item">
-                      <b>${card.views}</b>
-                    </p>
-                    <p class="info-item">
-                      <b>${card.comments}</b>
-                    </p>
-                    <p class="info-item">
-                      <b>${card.downloads}</b>
-                    </p>
-                  </div>
-                </div>
-                    `;
-                })
-            .join("");
-    divEl.innerHTML = markup;
-}
-
