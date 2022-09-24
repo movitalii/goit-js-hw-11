586069b23eef22528f27a19b1ac984d68760d98b
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-24 23:26:42 +0300

    add pagination

--- src/fetchPhotos.js
+++ src/fetchPhotos.js
@@ -1,14 +1,50 @@
 const API_KEY = '30062649-6c95f8a5f26546f2640c7031e';
 const BASE_URL = 'https://pixabay.com/api/';
 
+let page = 1;
+const query = [];
+
 function fetchPhotos(searchQuery) {
-    return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(response => {
+    if (searchQuery !== query[query.length - 1]) {
+                page = 1;
+            }
+    return fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
+        .then(response => {            
             if (!response.ok) {
                 throw new Error(response.status);
             }
-            return response.json();
+            query.push(searchQuery);         
+            page += 1;             
+            return response.json();            
         });
 };
 
 export { fetchPhotos };
+
+// export default class PixApiService {
+//     constructor() {
+//         this.data = '';
+//         this.page = 1;
+//     }
+
+//     fetchPhotos() {
+//         const API_KEY = '30062649-6c95f8a5f26546f2640c7031e';
+//         const BASE_URL = 'https://pixabay.com/api/';
+
+//         fetch(`${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=1&per_page=40`)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error(response.status);
+//             }
+//             return response.json();
+//         });
+//     }
+
+//     get query() {
+//         return this.searchQuery;
+//     }
+
+//     set query(newQuery) {
+//         this.searchQuery = newQuery;
+//     }
+// }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,57 @@
 const formEl = document.querySelector('#search-form');
 const divEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 formEl.addEventListener('submit', onFormSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
 import { fetchPhotos } from './fetchPhotos';
 
+let data = '';
+
 function onFormSubmit(event) {
-    event.preventDefault();
+  event.preventDefault();
+  
+  data = event.currentTarget.elements.searchQuery.value;
 
-    const data = event.currentTarget.elements.searchQuery.value;
+  fetchPhotos(data)
+    .then(data => {
+      console.log(data.hits);
+      return data.hits;
+    })
+    .then(renderCards)
+  .catch(error => console.log(error))
+}
 
-    fetchPhotos(data)
-    .then(data => { 
-        const elements = Object.entries(data);
-        const photosInfo = elements[elements.length - 1];
-        const photos = photosInfo[photosInfo.length - 1];
+function onLoadMore() {
+  fetchPhotos(data)
+    .then(data => {
+      console.log(data.hits);
+      return data.hits;
+    })
+    .then(renderCards)
+  .catch(error => console.log(error))
+}
 
-        const markup = photos.map((photo) => {
+function renderCards(photos) {
+  const markup = photos
+    .map((photo) => {
           return `<div class="photo-card">
           <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
           <div class="info">
-            <p class="info-item">
-              <b>Likes</b>
+            <p class="info-item">            
+              <b>Likes ${photo.likes}</b>
             </p>
             <p class="info-item">
-              <b>${photo.views}</b>
+              <b>Views ${photo.views}</b>
             </p>
             <p class="info-item">
-              <b>${photo.comments}</b>
+              <b>Comments ${photo.comments}</b>
             </p>
             <p class="info-item">
-              <b>${photo.downloads}</b>
+              <b>Downloades ${photo.downloads}</b>
             </p>
           </div>
         </div>`;
         })
         .join("");
-        divEl.innerHTML = markup;
-      })      
-    .catch(error => console.log(error))
+        divEl.insertAdjacentHTML('beforeend', markup);
 }
