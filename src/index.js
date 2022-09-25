264f8c3430ca6c93a3b0e2964876fc7534bd0803
Vitalii Mordovets <vitalii.mordovets@gmail.com>
2022-09-25 23:39:53 +0300

    add simpleLightbox

--- src/index.js
+++ src/index.js
@@ -1,29 +1,62 @@
 const formEl = document.querySelector('#search-form');
 const divEl = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+const submitBtn = document.querySelector('.submit');
 formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', onFormInput);
 loadMoreBtn.addEventListener('click', onLoadMore);
 import { fetchPhotos } from './fetchPhotos';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+let lightbox = new SimpleLightbox('.photo-card a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 let data = '';
 
-function onFormSubmit(event) {
+function onFormInput(event) {
+    submitBtn.removeAttribute('disabled');
+}
+
+loadMoreBtn.classList.add('is-hidden');
+
+function onFormSubmit(event) {  
   event.preventDefault();  
-  clearGalleryContainer()
+  clearGalleryContainer()  
+  loadMoreBtn.classList.add('is-hidden');  
 
   data = event.currentTarget.elements.searchQuery.value;
 
-  fetchPhotos(data)
-    .then(data => {      
-      return data.hits;
-    })
-    .then(renderCards)
+  if (data === '') {
+    Notiflix.Notify.warning('The field must not be empty!');
+  } else {
+    fetchPhotos(data)
+      .then(data => {
+        if (data.hits.length === 0) {
+          Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+        } else {
+          submitBtn.setAttribute('disabled', '');
+          loadMoreBtn.classList.remove('is-hidden');
+        }
+        return data.hits;
+      })
+      .then(renderCards)
   .catch(error => console.log(error))
+  }  
 }
 
 function onLoadMore() {
   fetchPhotos(data)
-    .then(data => {      
+    .then(data => {  
+      if (data.hits.length === 0) {
+        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        loadMoreBtn.classList.add('is-hidden');
+      }
       return data.hits;
     })
     .then(renderCards)
@@ -33,8 +66,10 @@ function onLoadMore() {
 function renderCards(photos) {
   const markup = photos
     .map((photo) => {
-          return `<div class="photo-card">
+      return `<div class="photo-card">
+          <a href="${photo.largeImageURL}">
           <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+          </a>
           <div class="info">
             <p class="info-item">            
               <b>Likes ${photo.likes}</b>
@@ -52,7 +87,8 @@ function renderCards(photos) {
         </div>`;
         })
         .join("");
-        divEl.insertAdjacentHTML('beforeend', markup);
+  divEl.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
 }
 
 function clearGalleryContainer() {
